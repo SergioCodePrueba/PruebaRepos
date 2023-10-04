e991a4501150b41d24b08e03a4736077a7788786
SergioCodePrueba <i202318899@cibertec.edu.pe>
2023-10-04 01:47:45 -0500

    dos

--- Moanso Github/Form1.Designer.cs
+++ Moanso Github/Form1.Designer.cs
@@ -71,7 +71,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(199, 171);
+            this.button1.Location = new System.Drawing.Point(189, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
